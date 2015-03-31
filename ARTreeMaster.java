f1a516f652d602497214c21c988f95824a1122bf
KeimaCheck <njpadinha@knights.ucf.edu>
2015-03-31 02:06:44 -0700

    Preparation for 2nd refactor
    ARTable will be split into two classes--ARTree and ARTreeMaster--and a
    new Traversal class will be used to condense the several different tree
    traversal operations into a single method taking a function object and a
    properties object as parameters.

---
+++ ARTreeMaster.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class ARTreeMaster here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ARTreeMaster
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class ARTreeMaster
+     */
+    public ARTreeMaster()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
